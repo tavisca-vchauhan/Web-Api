e3b9b030130776a1ce42f58cd6348e5490e17bee
Vishwas Chauhan <vchauhan@tavisca.com>
2018-09-04 11:12:38 +0530

    model

---
+++ CartDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    interface CartDetails
+    {
+        void TotalItemQuantity();
+        void TotalItemPrice();
+        void AddItemToCart();
+        void RemoveItemFromCart();
+        void ClearCart();
+        void MyCart();
+
+    }
+}
---
+++ MyCart.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    class MyCart : CartDetails , Mode
+    {
+        void PlaceOrder()
+        {
+
+        }
+        void ContinueShopping()
+        {
+
+        }
+
+        public void AddItemToCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItemFromCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TotalItemPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TotalItemQuantity()
+        {
+            throw new NotImplementedException();
+        }
+
+        void CartDetails.MyCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModeOfPurchase(string mode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
